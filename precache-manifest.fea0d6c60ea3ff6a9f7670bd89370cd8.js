285aff17fc37c95acb59eb98ee77f7fd1e2ecc1b
Mihirmathur <mihirmathur@g.ucla.edu>
2018-11-05 01:04:36 -0800

    Updates

---
+++ precache-manifest.fea0d6c60ea3ff6a9f7670bd89370cd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "30d66af715b0e0285c0e99a6db66ffe0",
+    "url": "/churro/static/media/red_rect.30d66af7.svg"
+  },
+  {
+    "revision": "0d1a0be555ea86536d0b20e479d613e9",
+    "url": "/churro/static/media/pattern.0d1a0be5.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/churro/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "45d805d796a8f5d3298126e819a69adb",
+    "url": "/churro/static/media/blue_rect.45d805d7.svg"
+  },
+  {
+    "revision": "cdbcaea7b8b41fad02d8",
+    "url": "/churro/static/js/runtime~main.cdbcaea7.js"
+  },
+  {
+    "revision": "9502dd0f2ae84d16fe5a",
+    "url": "/churro/static/js/main.9502dd0f.chunk.js"
+  },
+  {
+    "revision": "378847c83d028370d066",
+    "url": "/churro/static/js/1.378847c8.chunk.js"
+  },
+  {
+    "revision": "9502dd0f2ae84d16fe5a",
+    "url": "/churro/static/css/main.b676362a.chunk.css"
+  },
+  {
+    "revision": "832601969ea2e1b6908403e77f61ad5b",
+    "url": "/churro/index.html"
+  }
+];
