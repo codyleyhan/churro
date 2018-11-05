8f9c511db9250d12d68910d0df5940ccb5d29421
Mihirmathur <mihirmathur@g.ucla.edu>
2018-11-05 00:58:23 -0800

    created landing page

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,20 +7,14 @@ class Main extends Component {
   render() {
     return (
       <div className="Main">
-        <header className="Main-header">
-          <img src={logo} className="Main-logo" alt="logo" />
-          <p>
-            Edit <code>src/Main.js</code> and save to reload.
-          </p>
-          <a
-            className="Main-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <h2 className="welcome-header">Welcome to Churro!</h2>
+          <h4 className="welcome-subheader">The New Way to Manage Chores</h4>
+          <div className="button-container">
+            <button className="button button--new-group">START A NEW GROUP</button>
+            <button className="button button--login">LOGIN</button>
+          </div>
+        </div>
       </div>
     );
   }
