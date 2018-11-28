3799c528f48833b76274472a5fe2500f3ad11cd8
Cody Ley-Han <codyleyhan@gmail.com>
2018-11-28 12:36:17 -0800

    change login button if already logged in

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { observer } from "mobx-react";
 
 import CallToActionButton from './CallToActionButton';
 import Button from './Button';
 
+import userStore from '../stores/users';
+
 import '../styles/Main.scss';
 
-class Main extends Component {
+const Main = observer(class Main extends Component {
   transitionOut() {
     let elem = document.getElementById('landing');
     elem.style.left = "-10%";
@@ -22,13 +25,13 @@ class Main extends Component {
             <h4 className="welcome-subheader">The New Way to Manage Chores</h4>
             <div className="button-container">
               <Link to="/new"><CallToActionButton onClick={this.transitionOut}>START A NEW GROUP</CallToActionButton></Link>
-              <Link to="/login"><Button>LOGIN</Button></Link>
+              <Link to={userStore.isLoggedIn ? "/groups" : "/login"}><Button>{userStore.isLoggedIn ? "YOUR GROUPS" : "LOGIN"}</Button></Link>
             </div>
           </div>
         </div>
       </div>
     );
   }
-}
+});
 
 export default Main;
