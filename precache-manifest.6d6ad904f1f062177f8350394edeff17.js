28539191b2d16b5aeb80a52f89477c08a03447f1
Mihirmathur <mihirmathur@g.ucla.edu>
2018-11-29 15:06:03 -0800

    Updates

---
+++ precache-manifest.6d6ad904f1f062177f8350394edeff17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "30d66af715b0e0285c0e99a6db66ffe0",
+    "url": "/churro/static/media/red_rect.30d66af7.svg"
+  },
+  {
+    "revision": "0d1a0be555ea86536d0b20e479d613e9",
+    "url": "/churro/static/media/pattern.0d1a0be5.svg"
+  },
+  {
+    "revision": "45d805d796a8f5d3298126e819a69adb",
+    "url": "/churro/static/media/blue_rect.45d805d7.svg"
+  },
+  {
+    "revision": "cdbcaea7b8b41fad02d8",
+    "url": "/churro/static/js/runtime~main.cdbcaea7.js"
+  },
+  {
+    "revision": "718de1d9c4d760c253cc",
+    "url": "/churro/static/js/main.718de1d9.chunk.js"
+  },
+  {
+    "revision": "f9d1db9a30a0d2112166",
+    "url": "/churro/static/js/1.f9d1db9a.chunk.js"
+  },
+  {
+    "revision": "718de1d9c4d760c253cc",
+    "url": "/churro/static/css/main.e812109f.chunk.css"
+  },
+  {
+    "revision": "f9d1db9a30a0d2112166",
+    "url": "/churro/static/css/1.a9c97a5e.chunk.css"
+  },
+  {
+    "revision": "d89162cc95cafc8352c9d205984ef74d",
+    "url": "/churro/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/churro/precache-manifest.c331debbc3671187e631f0a251992bff.js"
+  "/churro/precache-manifest.6d6ad904f1f062177f8350394edeff17.js"
 );
 
 workbox.clientsClaim();
