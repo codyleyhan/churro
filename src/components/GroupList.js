a3bc9a3fa48b14eea07a0bea70e1a83be7f92c03
Hao Nguyen <haolnguyen7@gmail.com>
2018-11-28 22:20:19 -0800

    fixed nav

--- src/components/GroupList.js
+++ src/components/GroupList.js
@@ -1,56 +1,55 @@
-import React, { Component } from 'react';
-import { observer } from "mobx-react"
+import React, { Component } from "react";
+import { observer } from "mobx-react";
 import { Link } from "react-router-dom";
-import Spinner from 'react-spinkit';
+import Spinner from "react-spinkit";
 
+import NavBar from "./NavBar";
+import userStore from "../stores/users";
 
-import userStore from '../stores/users';
+import "../styles/Main.scss";
+import "../styles/NewGroup.scss";
 
-import '../styles/Main.scss';
-import '../styles/NewGroup.scss';
-
-const GroupList = observer(class GroupList extends Component {
-  constructor(props) {
-    super(props);
-    userStore.fetchCurrentUsersGroups();
-  }
-
-  render() {
-    let content = (
-      <div>
-        <Spinner name='double-bounce' />
-      </div>
-    );
-
-    if (!userStore.fetching) {
-      const groups = userStore.usersGroups.map(group => {
-        return (
-          <li key={group.id}>
-            <Link to={"/groups/" + group.id + "/tasks"}>{group.name}</Link>
-          </li>
-        )
-      });
-      content = (
-        <section>
-          <h1>Your Groups</h1>
-          <ul>
-            {groups}
-          </ul>
-        </section>
-      )
+const GroupList = observer(
+  class GroupList extends Component {
+    constructor(props) {
+      super(props);
+      userStore.fetchCurrentUsersGroups();
     }
-    return (
-      <div className="Main">
-        <div className="container">
-          <div className="NewGroup">
-          <div className="form group-name">
-            {content}
-          </div>
+
+    render() {
+      let content = (
+        <div>
+          <Spinner name="double-bounce" />
+        </div>
+      );
+
+      if (!userStore.fetching) {
+        const groups = userStore.usersGroups.map(group => {
+          return (
+            <li key={group.id}>
+              <Link to={"/groups/" + group.id + "/tasks"}>{group.name}</Link>
+            </li>
+          );
+        });
+        content = (
+          <section>
+            <h1>Your Groups</h1>
+            <ul>{groups}</ul>
+          </section>
+        );
+      }
+      return (
+        <div className="Main">
+          <NavBar />
+          <div className="container">
+            <div className="NewGroup">
+              <div className="form group-name">{content}</div>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-});
+);
 
 export default GroupList;
